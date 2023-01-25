68551233e86825a46627cdd941f3a92f1949eb0e
mertmusa <mmtemel41@gmail.com>
2023-01-25 19:10:37 +0300

    cpp01ex02

---
+++ CPP_Module_01/ex02/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/25 17:00:41 by mtemel            #+#    #+#             */
+/*   Updated: 2023/01/25 19:06:41 by mtemel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+	//
+	std::cout<<"\033[1;34m"<<std::setw(45)<<std::left<<"The memory address of the string variable"<<":"<<"\033[0m";
+	std::cout<<&str<<std::endl;
+	std::cout<<"\033[1;34m"<<std::setw(45)<<std::left<<"The memory address held by stringPTR"<<":"<<"\033[0m";
+	std::cout<<stringPTR<<std::endl;
+	std::cout<<"\033[1;34m"<<std::setw(45)<<std::left<<"The memory address held by stringREF"<<":"<<"\033[0m";
+	std::cout<<&stringREF<<std::endl;
+	//
+	std::cout<<"\033[1;34m"<<std::setw(45)<<std::left<<"The value of the string variable"<<":"<<"\033[0m";
+	std::cout<<str<<std::endl;
+	std::cout<<"\033[1;34m"<<std::setw(45)<<std::left<<"The value pointed to by stringPTR"<<":"<<"\033[0m";
+	std::cout<<*stringPTR<<std::endl;
+	std::cout<<"\033[1;34m"<<std::setw(45)<<std::left<<"The value pointed to by stringREF"<<":"<<"\033[0m";
+	std::cout<<stringREF<<std::endl;
+}
