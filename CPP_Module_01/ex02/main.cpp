a93347809ab4bcdc881c871d5cac2fe46155f3ab
mertmusa <mmtemel41@gmail.com>
2023-01-25 19:13:19 +0300

    cpp01ex02

--- CPP_Module_01/ex02/main.cpp
+++ CPP_Module_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 17:00:41 by mtemel            #+#    #+#             */
-/*   Updated: 2023/01/25 19:06:41 by mtemel           ###   ########.fr       */
+/*   Updated: 2023/01/25 19:12:37 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ int main()
 	std::cout<<stringPTR<<std::endl;
 	std::cout<<"\033[1;34m"<<std::setw(45)<<std::left<<"The memory address held by stringREF"<<":"<<"\033[0m";
 	std::cout<<&stringREF<<std::endl;
+	//
+	std::cout<<std::setw(15)<<std::right<<"-----"<<std::endl;
 	//
 	std::cout<<"\033[1;34m"<<std::setw(45)<<std::left<<"The value of the string variable"<<":"<<"\033[0m";
 	std::cout<<str<<std::endl;
