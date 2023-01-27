6c9454540ddd989fcd71b7a31ba615f5bd5bd739
Mert musa Temel <mtemel@k2m08s08.42kocaeli.com.tr>
2023-01-27 19:27:43 +0300

    leaks

--- CPP_Module_01/ex00/main.cpp
+++ CPP_Module_01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 19:23:54 by mtemel            #+#    #+#             */
-/*   Updated: 2023/01/25 00:18:06 by mtemel           ###   ########.fr       */
+/*   Updated: 2023/01/27 19:11:04 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ int main()
 	NewOne->randomChump("Foo");
 	NewOne->randomChump("Bar");
 	delete NewOne;
-	//while(1);
+	//system("leaks zombie");
 }
--- CPP_Module_01/ex01/main.cpp
+++ CPP_Module_01/ex01/main.cpp
@@ -6,23 +6,17 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 01:39:55 by mtemel            #+#    #+#             */
-/*   Updated: 2023/01/27 18:42:37 by mtemel           ###   ########.fr       */
+/*   Updated: 2023/01/27 19:08:01 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <unistd.h>
 
-void	a()
+int main()
 {
 	Zombie* NewOne = new Zombie;
 	NewOne->zombieHorde(4, "Canavar");
 	delete NewOne;
-}
-
-int main()
-{
-	a();
-	while (1);
-		//;
+	//system("leaks zombiehorde");
 }
