9fbfdda3241feb3ec47301cbeb4d0ecc345e5b7a
mertmusa <mmtemel41@gmail.com>
2023-02-01 16:28:29 +0300

    cpp02ex01

--- CPP_Module_02/ex01/Fixed.cpp
+++ CPP_Module_02/ex01/Fixed.cpp
@@ -6,52 +6,34 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 16:16:55 by mtemel            #+#    #+#             */
-/*   Updated: 2023/02/01 15:04:30 by mtemel           ###   ########.fr       */
+/*   Updated: 2023/02/01 16:18:22 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+//ex00
 int	Fixed::getRawBits(void) const
 {
-	std::cout<<"getRawBits member function called"<<std::endl;
-	return (this->FixedVal);
+	//std::cout<<"getRawBits member function called"<<std::endl;
+	return (this->fixedval);
 }
 
+//ex00
 void	Fixed::setRawBits(int const raw)
 {
 	//std::cout<<"setRawBits member function called"<<std::endl;
-	this->FixedVal = raw;
+	this->fixedval = raw;
 }
 
-float	Fixed::toFlaot(void)
-{
-	return (roundf((float)(this->fixedval) / (float)(1 << this->fixfrac)));
-}
-
-int	Fixed::toInt(void)
-{
-	return (fixedval / 1<<fixfrac);
-}
-
+//ex00
 Fixed::Fixed(void)
 {
 	std::cout<<"Default constructor called"<<std::endl;
-	this->FixedVal = 0;
+	this->fixedval = 0;
 }
 
-Fixed::Fixed(const int conint)
-{
-	std::cout<<"Int constructor called"<<std::endl;
-	this->fixedval = conint * (1 << this->fixfrac);
-}
-
-Fixed::Fixed(const float conflo)
-{
-	std::cout<<"Float constructor called"<<std::endl;
-	this->fixedval = roundf(conflo * (1 << this->fixfrac));
-}
-
+//ex00
 Fixed::Fixed(const Fixed& mfcopy)
 {
 	std::cout<<"Copy constructor called"<<std::endl;
@@ -59,6 +41,7 @@ Fixed::Fixed(const Fixed& mfcopy)
 	*this = mfcopy;
 }
 
+//ex00
 Fixed& Fixed::operator = (const Fixed& mfcopy)
 {
 	std::cout<<"Copy assignment operator called"<<std::endl;
@@ -66,15 +49,56 @@ Fixed& Fixed::operator = (const Fixed& mfcopy)
 	return (*this);
 }
 
-std::ostream& Fixed::operator<<(std::ostream& o, const Fixed& mfcopy)
-{
-	//std::cout<<"Copy assignment operator called"<<std::endl;
-	//mfcopy.toFlaot();
-	o << Fixed.toFloat();
-	return (o);
-}
-
+//ex00
 Fixed::~Fixed()
 {
 	std::cout<<"Destructor called"<<std::endl;
+}
+
+//ex00 -> ex01
+
+//ex01
+Fixed::Fixed(const int conint)
+{
+	std::cout<<"Int constructor called"<<std::endl;
+	//this->fixedval = conint * (1 << this->fixfrac);
+	//setRawBits(conint << this->fixfrac);
+	fixedval = (conint << this->fixfrac);
+}
+
+//ex01
+Fixed::Fixed(const float conflo)
+{
+	std::cout<<"Float constructor called"<<std::endl;
+	//this->fixedval = roundf(conflo * (1 << this->fixfrac));
+	//this->setRawBits((int)roundf(conflo * (1 << this->fixfrac)));
+	//setRawBits((int)roundf(conflo * (1 << this->fixfrac)));
+	//fixedval = (int)roundf(conflo * (1 << this->fixfrac));
+	fixedval = roundf(conflo * (1 << this->fixfrac));
+}
+
+//ex01
+float	Fixed::toFloat(void) const
+{
+	//return (roundf((float)(this->fixedval) / (float)(1 << this->fixfrac)));
+	//return ((float)(this->getRawBits()) / (1 << this->fixfrac));
+	return ((float)(fixedval) / (1 << fixfrac));
+}
+
+//ex01
+int	Fixed::toInt(void) const
+{
+	//return (fixedval / 1<<fixfrac);
+	//return (this->getRawBits() / (1 << this->fixfrac));
+	//return (this->getRawBits() >> this->fixfrac);
+	return (fixedval >> fixfrac);
+}
+
+//ex01
+std::ostream& operator<<(std::ostream& ost, const Fixed& mfcopy)
+{
+	//std::cout<<"Copy assignment operator called"<<std::endl;
+	//mfcopy.toFlaot();
+	ost << mfcopy.toFloat();
+	return (ost);
 }
--- CPP_Module_02/ex01/Fixed.hpp
+++ CPP_Module_02/ex01/Fixed.hpp
@@ -6,9 +6,12 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 16:13:22 by mtemel            #+#    #+#             */
-/*   Updated: 2023/02/01 01:53:51 by mtemel           ###   ########.fr       */
+/*   Updated: 2023/02/01 15:37:16 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
 
 #include <iostream>
 #include <cmath>
@@ -18,14 +21,20 @@ class Fixed{
 		int fixedval;
 		static const int fixfrac = 8;
 	public:
+		//ex00
 		Fixed(void);
-		Fixed(const int conint);
-		Fixed(const float conflo);
 		Fixed(const Fixed& mfcopy);
 		Fixed& operator = (const Fixed& mfcopy);
 		~Fixed(void);
 		int getRawBits( void ) const;
 		void setRawBits( int const raw );
-		float toFlaot(void);
-		int toInt(void);
+		//ex01
+		Fixed(const int conint);
+		Fixed(const float conflo);
+		float toFloat(void) const;
+		int toInt(void) const;
 };
+
+std::ostream& operator<<(std::ostream& ost, const Fixed& mfcopy);
+
+#endif
