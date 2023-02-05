f923118981a22cf86f53145b9d90abb822c9be62
mertmusa <mmtemel41@gmail.com>
2023-02-05 23:12:26 +0300

    inherit constructor

--- CPP_Module_03/ex03/DiamondTrap.cpp
+++ CPP_Module_03/ex03/DiamondTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 19:41:58 by mtemel            #+#    #+#             */
-/*   Updated: 2023/02/05 22:55:31 by mtemel           ###   ########.fr       */
+/*   Updated: 2023/02/05 23:09:18 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ DiamondTrap::DiamondTrap()
 	std::cout<<"\033[1;36mDiamondTrap Default Constructor Called!\033[0m"<<std::endl;
 }
 
-DiamondTrap::DiamondTrap(std::string sName) : ScavTrap(sName)
+DiamondTrap::DiamondTrap(std::string sName) : ScavTrap(sName), FragTrap(sName)
 {
 	this->name = ScavTrap::getName();
 	this->hitPoint = FragTrap::getHit();
@@ -36,5 +36,5 @@ void DiamondTrap::attack(const std::string& target)
 
 void DiamondTrap::whoAmI()
 {
-	std::cout<<"\033[1;36mDiamondTrap's name: "<<this->name<<" ClapTrap's name: "<<ScavTrap::getName()<<"!\033[0m"<<std::endl;
+	std::cout<<"\033[1;36mwhoAmI? DiamondTrap's name: "<<this->name<<" ClapTrap's name: "<<ScavTrap::getName()<<"!\033[0m"<<std::endl;
 }
