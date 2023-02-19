c03536333eaf6899f174a796553a72f1d7315fb4
mertmusa <mmtemel41@gmail.com>
2023-02-19 03:03:23 +0300

    cpp07ex02

---
+++ CPP_Module_07/ex02/Array.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/18 23:45:05 by mtemel            #+#    #+#             */
+/*   Updated: 2023/02/19 02:58:07 by mtemel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <exception>
+
+template<typename T>
+class Array{
+	private:
+		int size;
+		T* data;
+	public:
+		Array() : size(0), data(new T[0]) {}
+		Array(unsigned int n) : size(n) , data(new T[n]) {}
+		Array (const Array& acopy)
+		{
+			*this = acopy;
+		}
+		Array operator = (Array const)
+		{
+			return *this;
+		}
+		T operator[](int index)
+		{
+			if (index >= 0 && index < size)
+				return (data[index]);
+			else
+				throw OutOfBound();
+		}
+		~Array()
+		{
+			delete[] data;
+		}
+		class OutOfBound : public std::exception{
+			public:
+				virtual const char* what() const throw() {
+					return ("\033[1;31mIndex is out of bound!\033[0m");
+				}
+		};
+};
