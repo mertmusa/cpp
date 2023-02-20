8882e73549d2a61e1b64b8a7835980e2b7abddb3
mertmusa <mmtemel41@gmail.com>
2023-02-20 23:04:34 +0300

    cpp08ex00

---
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/20 23:00:54 by mtemel            #+#    #+#             */
+/*   Updated: 2023/02/20 23:04:00 by mtemel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+template<typename T>
+easyfind(T haystack, int needle)
+{
+	//find needle in haystack
+	//if there is none, throw an exception or error value
+}
