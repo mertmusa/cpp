b9074d56eb4277f746b01bfacec866e11228b3e6
mertmusa <mmtemel41@gmail.com>
2023-02-20 23:22:20 +0300

    started easyfind

--- CPP_Module_08/ex00/easyfind.hpp
+++ CPP_Module_08/ex00/easyfind.hpp
@@ -6,15 +6,34 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 23:00:54 by mtemel            #+#    #+#             */
-/*   Updated: 2023/02/20 23:04:00 by mtemel           ###   ########.fr       */
+/*   Updated: 2023/02/20 23:21:22 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+
+class NotFound : public std::exception{
+	public:
+		virtual const char* what() const throw() {
+			return ("\033[1;31mContainer doesn't contain the given number!\033[0m");
+		}
+};
 
 template<typename T>
 easyfind(T haystack, int needle)
 {
 	//find needle in haystack
 	//if there is none, throw an exception or error value
+	int hay_size = sizeof(haystack) / sizeof(int);
+	int i = 0;
+	while (i < hay_size)
+	{
+		if (haystack[i] == needle)
+			return(haystack[i]);
+		 i++;
+	}
+	if (i >= hay_size)
+	{
+		throw NotFound();
+	}
 }
