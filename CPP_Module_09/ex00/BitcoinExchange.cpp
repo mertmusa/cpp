ccf9ac47825f950f0b13160c975144389bb05857
mtemel <mmtemel41@gmail.com>
2023-03-28 17:00:17 +0300

    will add time variable

--- CPP_Module_09/ex00/BitcoinExchange.cpp
+++ CPP_Module_09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 15:50:12 by mtemel            #+#    #+#             */
-/*   Updated: 2023/03/27 16:31:37 by mtemel           ###   ########.fr       */
+/*   Updated: 2023/03/28 16:59:34 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,19 @@
 
 BitcoinExchange::BitcoinExchange()
 {
-	ifs.open("../cpp_09/data.csv", std::ifstream::in);
-	if(!ifs.is_open())
-		std::cout << "CVS could not open!" << std::endl;
+	std::string line;
+	this->ifs.open("../cpp_09/data.csv", std::ifstream::in);
+	if(!this->ifs.is_open())
+		std::cout << "\033[1;31mCVS could not open!\033[0m" << std::endl;
 	else
-		std::cout << "CVS opened properly!" << std::endl;
+		std::cout << "\033[1;32mCVS opened properly!\033[0m" << std::endl;
+	std::getline(this->ifs, line); // first line : (date, exchange_rate)
+	// std::cout << "\033[1;34mLine: \033[0m" << line << std::endl;
+	while (std::getline(this->ifs, line))
+	{
+		//add time property
+		//https://stackoverflow.com/questions/32523230/how-convert-string-to-datetime-c
+	}
 }
 
 BitcoinExchange::~BitcoinExchange()
@@ -34,4 +42,9 @@ BitcoinExchange::BitcoinExchange(const BitcoinExchange& copy)
 BitcoinExchange BitcoinExchange::operator = (BitcoinExchange const&)
 {
 	return (*this);
+}
+
+std::map<std::string, float> BitcoinExchange::getMap()
+{
+	return (this->map);
 }
--- CPP_Module_09/ex00/BitcoinExchange.hpp
+++ CPP_Module_09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 15:50:12 by mtemel            #+#    #+#             */
-/*   Updated: 2023/03/27 16:28:19 by mtemel           ###   ########.fr       */
+/*   Updated: 2023/03/28 16:43:27 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@ class BitcoinExchange
 	private:
 		std::map<std::string, float> map;
 		std::ifstream ifs;
-		std::string line;
 	public:
 		BitcoinExchange();
 		BitcoinExchange(const BitcoinExchange& copy);
 		BitcoinExchange operator = (const BitcoinExchange& copy);
 		~BitcoinExchange();
+		std::map<std::string, float> getMap();
 };
