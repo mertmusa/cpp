2a72db0dadc025fcab9357da9b2ba4dae17eff9f
mtemel <mmtemel41@gmail.com>
2023-03-30 19:30:18 +0300

    header protection

--- CPP_Module_09/ex00/BitcoinExchange.hpp
+++ CPP_Module_09/ex00/BitcoinExchange.hpp
@@ -6,9 +6,12 @@
 /*   By: mtemel <mtemel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 15:50:12 by mtemel            #+#    #+#             */
-/*   Updated: 2023/03/30 01:58:48 by mtemel           ###   ########.fr       */
+/*   Updated: 2023/03/30 19:29:37 by mtemel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
 
 #include <iostream>
 #include <fstream>
@@ -31,3 +34,5 @@ class BitcoinExchange
 		std::map<int, float> getMap();
 		void rateCalculator(char *filename);
 };
+
+#endif
